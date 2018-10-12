d27723b7a685fee5e3ecd1398af54b2f0101ad05
Zac <zombiezac@att.net>
2018-10-12 12:58:28 -0600

    added some getters and setter and some basic framework code

---
+++ src/controller/ChatController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import model.ChatBot;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+public class ChatController
+{
+	private ChatBot simpleBot;
+	
+	public ChatController()
+	{
+		simpleBot = new ChatBot();
+	}
+
+	public void interactWithChatBot(String userInput)
+	{
+		
+		
+	}
+	
+	
+	
+	public void start()
+	{
+		
+	}
+	
+}
